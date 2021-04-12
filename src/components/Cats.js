c3b8f0446ab67df384e4729f9595c25eb7350996
ethanfincher <ethan.j.fincher@gmail.com>
2021-04-12 08:09:14 -0500

    added for cats now too

--- src/components/Cats.js
+++ src/components/Cats.js
@@ -3,10 +3,14 @@ import PetList from './PetList.js';
 
 export default function Cats({ token }) {
 	const [catsList, setCatsList] = useState([]);
+    const [location, setLocation] = useState('60014');
 
 	function getCats() {
 		var xhr = new XMLHttpRequest();
-		xhr.open('GET', 'https://api.petfinder.com/v2/animals?type=Cat');
+		xhr.open(
+			'GET',
+			`https://api.petfinder.com/v2/animals?type=Cat&status=adoptable&location=${location}`
+		);
 		xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 		xhr.setRequestHeader('Authorization', 'Bearer ' + token);
 		xhr.onreadystatechange = function () {
@@ -21,12 +25,29 @@ export default function Cats({ token }) {
 		};
 		xhr.send(null);
 	}
+
+    function onChange(event) {
+			setLocation(event.target.value);
+			console.log(location);
+		}
+		function onSubmit(event) {
+			event.preventDefault();
+			console.log(location);
+			getCats();
+		}
+
 	useEffect(() => {
 		getCats();
 	}, []);
 
 	return (
 		<div>
+			<form onSubmit={onSubmit}>
+				<label>Enter your zip code here to find pets around you!</label>
+				<br />
+				<input type='text' onChange={onChange} placeholder='ZIP'></input>
+				<button type='submit'>Find a Friend!</button>
+			</form>
 			<PetList list={catsList} type='cat' />
 		</div>
 	);
